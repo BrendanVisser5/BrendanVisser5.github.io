e4c26397da6be5277fa42d128d750963a7c4302a
unknown <brendanvisser@msn.com>
2022-07-18 19:46:40 +0200

    web updates

---
+++ anime.js
@@ -0,0 +1,46 @@
+var path = anime.path('path');
+
+let AnimateLogo1 = anime({
+  targets: '.logo.one',
+  translateX: path('x'),
+  translateY: path('y'),
+  easing: 'linear',
+  duration: 7000,
+  loop: true
+});
+
+let AnimateLogo2 = anime({
+  targets: '.logo.two',
+  translateX: path('x'),
+  translateY: path('y'),
+  easing: 'linear',
+  duration: 8000,
+  loop: true
+});
+
+let AnimateLogo3 =  anime({
+  targets: '.logo.three',
+  translateX: path('x'),
+  translateY: path('y'),
+  easing: 'linear',
+  duration: 9000,
+  loop: true
+});
+
+let AnimateLogo4 = anime({
+  targets: '.logo.four',
+  translateX: path('x'),
+  translateY: path('y'),
+  easing: 'linear',
+  duration: 10000,
+  loop: true
+});
+
+let AnimateLogo5 = anime({
+  targets: '.logo.five',
+  translateX: path('x'),
+  translateY: path('y'),
+  easing: 'linear',
+  duration: 11000,
+  loop: true
+});
